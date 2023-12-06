57adffe009198376b60eb76a1281293337d6b3ce
Kirubel141797 <kirubelteshome22@gmail.com>
2023-12-06 11:53:42 -0800

    soo rizz

--- models/base_models.py
+++ models/base_models.py
@@ -5,3 +5,18 @@ This is the base model of the for airbnb clone given as a task by ALX.
 import uuid
 from datetime import datetime
 from models import storage
+
+class BaseModel:
+    """
+    This is the base model for the airbnb clone.
+    """
+    def __init__(self, *args, **kwargs):
+        """
+        initializes the base model object.
+        """
+        if kwargs and len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
