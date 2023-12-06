c30c50718482dd9e484c05dcf2a0b45755f50be6
Kirubel141797 <kirubelteshome22@gmail.com>
2023-12-06 12:26:57 -0800

    soo rizz

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""
+This program handles the test cases for base_model of airbnb clone.
+"""
+from models.base_model import BaseModel
+import unittest
+from datetime import datetime
