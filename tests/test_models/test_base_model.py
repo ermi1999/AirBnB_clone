7687f999c266c5b730c67ce42edfb3fae2b894d7
Kirubel141797 <kirubelteshome22@gmail.com>
2023-12-06 12:29:22 -0800

    soo rizz

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -5,3 +5,11 @@ This program handles the test cases for base_model of airbnb clone.
 from models.base_model import BaseModel
 import unittest
 from datetime import datetime
+
+class TestBaseModel(unittest.TestCase):
+    """
+    This object handles the test cases for BaseModel object.
+    """
+    def test_id(self):
+        base = BaseModel()
+        self.assertIsInstance(base.id, str)
